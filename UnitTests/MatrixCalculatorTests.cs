01680f200462fe2bc9eb54c76925fc84aa9e7613
BedDuckey <artyomkos2002@yandex.ru>
2025-04-13 17:09:35 +0300

    Добавьте файлы проекта.

---
+++ UnitTests/MatrixCalculatorTests.cs
@@ -0,0 +1,104 @@
+﻿using Xunit;
+using MatrixCalc;
+using System;
+
+namespace MatrixTests.UnitTests
+{
+    public class MatrixCalculatorTests
+    {
+        private readonly IMatrixCalc _calculator;
+
+        public MatrixCalculatorTests()
+        {
+            _calculator = new MyMatrixCalculator();
+        }
+
+        [Fact]
+        public void Add_ValidMatrices_ReturnsCorrectSum()
+        {
+            // Arrange
+            var matrixA = new double[,] { { 1, 2 }, { 3, 4 } };
+            var matrixB = new double[,] { { 5, 6 }, { 7, 8 } };
+            var expected = new double[,] { { 6, 8 }, { 10, 12 } };
+
+            // Act
+            var result = _calculator.Add(matrixA, matrixB);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Add_DifferentDimensions_ThrowsArgumentException()
+        {
+            // Arrange
+            var matrixA = new double[,] { { 1, 2 } };
+            var matrixB = new double[,] { { 1, 2, 3 } };
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _calculator.Add(matrixA, matrixB));
+        }
+
+        [Fact]
+        public void Multiply_ValidMatrices_ReturnsCorrectProduct()
+        {
+            // Arrange
+            var matrixA = new double[,] { { 1, 2 }, { 3, 4 } };
+            var matrixB = new double[,] { { 2, 0 }, { 1, 2 } };
+            var expected = new double[,] { { 4, 4 }, { 10, 8 } };
+
+            // Act
+            var result = _calculator.Multiply(matrixA, matrixB);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Multiply_IncompatibleDimensions_ThrowsArgumentException()
+        {
+            // Arrange
+            var matrixA = new double[,] { { 1, 2, 3 } };
+            var matrixB = new double[,] { { 1, 2 } };
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _calculator.Multiply(matrixA, matrixB));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2.5)]
+        [InlineData(-3)]
+        public void MultiplyByScalar_ValidInput_ReturnsCorrectResult(double scalar)
+        {
+            // Arrange
+            var matrix = new double[,] { { 1, 2 }, { 3, 4 } };
+            var expected = new double[,]
+            {
+            { 1 * scalar, 2 * scalar },
+            { 3 * scalar, 4 * scalar }
+            };
+
+            // Act
+            var result = _calculator.MultiplyByScalar(matrix, scalar);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Transpose_SquareMatrix_ReturnsCorrectResult()
+        {
+            // Arrange
+            var matrix = new double[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+            var expected = new double[,] { { 1, 4 }, { 2, 5 }, { 3, 6 } };
+
+            // Act
+            var result = _calculator.Transpose(matrix);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
+
---
+++ UnitTests/MockTests.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+using Moq;
+using MatrixCalc;
+using System;
+
+namespace MatrixTests.UnitTests
+{
+
+    public class MockMatrixCalculatorTests
+    {
+        private readonly Mock<IMatrixCalc> _mockCalculator;
+
+        public MockMatrixCalculatorTests()
+        {
+            _mockCalculator = new Mock<IMatrixCalc>();
+        }
+
+        [Fact]
+        public void Mock_Add_ReturnsPredefinedResult()
+        {
+            // Arrange
+            var expected = new double[,] { { 1, 1 }, { 1, 1 } };
+            _mockCalculator.Setup(x => x.Add(It.IsAny<double[,]>(), It.IsAny<double[,]>()))
+                          .Returns(expected);
+
+            // Act
+            var result = _mockCalculator.Object.Add(new double[2, 2], new double[2, 2]);
+
+            // Assert
+            Assert.Equal(expected, result);
+            _mockCalculator.Verify(x => x.Add(It.IsAny<double[,]>(), It.IsAny<double[,]>()), Times.Once);
+        }
+
+        [Fact]
+        public void Mock_Multiply_VerifiesParameters()
+        {
+            // Arrange
+            var testMatrix = new double[,] { { 1, 2 } };
+            _mockCalculator.Setup(x => x.Multiply(It.IsAny<double[,]>(), It.IsAny<double[,]>()))
+                          .Returns(new double[1, 1]);
+
+            // Act
+            _mockCalculator.Object.Multiply(testMatrix, testMatrix);
+
+            // Assert
+            _mockCalculator.Verify(x => x.Multiply(
+                It.Is<double[,]>(m => m.GetLength(0) == 1 && m.GetLength(1) == 2),
+                It.IsAny<double[,]>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public void Mock_Add_ThrowsException()
+        {
+            // Arrange
+            _mockCalculator.Setup(x => x.Add(It.IsAny<double[,]>(), It.IsAny<double[,]>()))
+                          .Throws<ArgumentException>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+                _mockCalculator.Object.Add(new double[1, 2], new double[2, 2]));
+        }
+
+        [Fact]
+        public void Mock_MultipleOperations_VerifySequence()
+        {
+            // Arrange
+            var matrix = new double[,] { { 1 } };
+            _mockCalculator.Setup(x => x.Add(It.IsAny<double[,]>(), It.IsAny<double[,]>()))
+                          .Returns(matrix);
+            _mockCalculator.Setup(x => x.MultiplyByScalar(It.IsAny<double[,]>(), It.IsAny<double>()))
+                          .Returns(matrix);
+
+            // Act
+            var addResult = _mockCalculator.Object.Add(matrix, matrix);
+            var scalarResult = _mockCalculator.Object.MultiplyByScalar(matrix, 2);
+
+            // Assert
+            _mockCalculator.Verify(x => x.Add(matrix, matrix), Times.Once);
+            _mockCalculator.Verify(x => x.MultiplyByScalar(matrix, 2), Times.Once);
+        }
+    }
+}
